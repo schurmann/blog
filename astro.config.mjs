9da1f65e31733954cc0ae9c2b33e4228ada941c2
Jonathan Schurmann <jonathan.schurmann@sinch.com>
2022-09-09 19:21:07 +0200

    feat: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind({
     config: {
-      applyBaseStyles: false,
+      applyBaseStyles: false
     }
-  })]
+  })],
+  output: "server",
+  adapter: netlify()
 });
