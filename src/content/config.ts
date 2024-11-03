a223357c92f59365910701f23835e38bad1354c7
Jonathan Schurmann <jonathan@bifrostsec.com>
2024-11-03 14:35:59 +0100

    feat: astro 5

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,20 @@
 import {defineCollection, z} from "astro:content";
-const blogCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        pubDate: z.date(),
-        description: z.string(),
-        heroImage: z.string().optional(),
-    })
+import {glob} from "astro/loaders";
+
+const schema = z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    heroImage: z.string().optional(),
 });
+
+export type Blog = z.infer<typeof schema>;
+
+const blogCollection = defineCollection({
+    loader: glob({pattern: '**\/[^_]*.mdx', base: "./src/data/blog"}),
+    schema,
+});
+
 export const collections = {
     blog: blogCollection,
 };
